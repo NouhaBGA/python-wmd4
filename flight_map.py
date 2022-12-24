7af9587d2cd6ee36dee27dafebb321e2e4b70ade
NouhaBGA <92851443+NouhaBGA@users.noreply.github.com>
2022-12-24 22:06:44 +0100

    question J et K

--- flight_map.py
+++ flight_map.py
@@ -2,7 +2,9 @@ import csv
 import re
 from flight import Flight
 from airport import Airport
-from typing import List
+from typing import List, Tuple, Dict
+from flight_path import FlightPath
+from collections import deque
 
 
 class FlightMap:
@@ -77,6 +79,103 @@ class FlightMap:
                 destinations.add(f[1])
         return destinations
 
+    def paths(self, src_airport_code: str, dst_airport_code: str) -> List[FlightPath]:
+        paths = []
+
+        src_airport = self.airport_list.get(src_airport_code)
+        dst_airport = self.airport_list.get(dst_airport_code)
+
+        airports_not_visited = set(self.airport_list.values())
+        airports_future = deque([src_airport])
+        airports_visited = set()
+
+        while airports_future:
+            airport = airports_future.popleft()
+            airports_not_visited.remove(airport)
+            airports_visited.add(airport)
+
+            if airport == dst_airport:
+                paths.append(FlightPath(src_airport))
+            else:
+                for nearby_airport in self._find_nearby_airports(airport):
+                    if nearby_airport not in airports_visited:
+                        airports_future.append(nearby_airport)
+                        airports_not_visited.remove(nearby_airport)
+                        airports_visited.add(nearby_airport)
+
+        return paths
+
+    def _find_nearby_airports(self, airport: Airport) -> List[Airport]:
+        nearby_airports = []
+        for code, airport in self.airport_list.items():
+            if (airport.code, code) in self.flight_list:
+                nearby_airports.append(airport)
+        return nearby_airports
+
+    def paths_shortest_length(self, src_airport_code: str, dst_airport_code: str) -> List[FlightPath]:
+        paths = []
+
+        src_airport = self.airport_list.get(src_airport_code)
+        dst_airport = self.airport_list.get(dst_airport_code)
+
+        # Initialise la liste des aéroports à visiter et la distance de chaque aéroport
+        airports_future = deque([(src_airport, 0)])
+        distances = {src_airport: 0}
+        # Initialise la liste des aéroports visités et non visités
+        airports_visited = set()
+        airports_not_visited = set(self.airport_list.values())
+
+        # Tant qu'il reste des aéroports à visiter
+        while airports_future:
+            # Récupère l'aéroport et sa distance
+            airport, distance = airports_future.popleft()
+            # Si l'aéroport courant est la destination, ajoute un nouveau chemin à la liste et continue la recherche
+            if airport == dst_airport:
+                path = self.construct_path(src_airport, dst_airport, distances)
+                paths.append(path)
+            # Sinon, récupère les aéroports accessibles depuis l'aéroport courant
+            next_airports = self.get_next_airports(airport, distance, dst_airport)
+            # Met à jour la liste des aéroports à visiter et la distance de chaque aéroport
+            for next_airport, next_distance in next_airports:
+                if next_airport not in airports_visited:
+                    airports_future.append((next_airport, next_distance))
+                    distances[next_airport] = next_distance
+            # Marque l'aéroport courant comme visité
+            airports_not_visited.remove(airport)
+            airports_visited.add(airport)
+
+        # Garde seulement les chemins de longueur minimale
+        if paths:
+            min_length = min(len(path) for path in paths)
+            paths = [path for path in paths if len(path) == min_length]
+        else:
+            paths = []
+
+        # Retourne la liste des chemins trouvés
+        return paths
+
+        # Retourne la liste des chemins trouvés
+        return paths
+
+    def get_next_airports(self, airport: Airport, distance: int, dst_airport: Airport) -> List[Tuple[Airport, int]]:
+        next_airports = []
+        for code, next_airport in self.airport_list.items():
+            if (airport.code, code) in self.flight_list:
+                next_airports.append((next_airport, distance + 1))
+        return next_airports
+
+    def construct_path(self, src_airport: Airport, dst_airport: Airport, distances: Dict[Airport, int]) -> FlightPath:
+        path = [dst_airport]
+        current_airport = dst_airport
+        while current_airport != src_airport:
+            for code, airport in self.airport_list.items():
+                if (code, current_airport.code) in self.flight_list and distances[airport] == distances[current_airport] \
+                        - 1:
+                    path.insert(0, airport)
+                    current_airport = airport
+                    break
+        return FlightPath(path)
+
 
 flight_map = FlightMap()
 flight_map.import_airports("aeroports.csv")
@@ -98,8 +197,8 @@ flight_map.import_flights("flights.csv")
 
 
 # Question F
-print(flight_map.flight_exist("SYD", "NBO"))  # True
-print(flight_map.flight_exist("MIA", "DFW"))  # False
+# print(flight_map.flight_exist("SYD", "NBO"))  # True
+# print(flight_map.flight_exist("MIA", "DFW"))  # False
 
 # Question G
 flights = flight_map.flights_where("ICN")
@@ -109,6 +208,9 @@ for my_flight in flights:
 
 airports = flight_map.airports_from("CDG")
 print(airports)
+
+print(flight_map.paths("BNE", "BNO"))
+print(flight_map.paths_shortest_length("BNE", "BNO"))
 
 
 
--- flight_path.py
+++ flight_path.py
@@ -1,6 +1,6 @@
-from flight_map import FlightMap
 from flight import Flight
 from airport import Airport
+from typing import List
 
 
 class FlightPathBroken(Exception):
@@ -12,4 +12,25 @@ class FlightPathDuplicate(Exception):
 
 
 class FlightPath:
-    pass
+    def __init__(self, src_airport: Airport):
+        self.l_flights = []
+        self.l_airports = [src_airport]
+
+    def add(self, dst_airport: Airport, via_flight: Flight):
+        if self.l_airports[-1] != via_flight.src_airport:
+            raise FlightPathBroken("Error: flight does not depart from last airport in the path")
+        self.l_flights.append(via_flight)
+        self.l_airports.append(dst_airport)
+
+    def flights(self) -> List[Flight]:
+        return self.l_flights
+
+    def airports(self) -> List[Airport]:
+        return self.l_airports
+
+    def steps(self) -> int:
+        return len(self.l_flights)
+
+    def duration(self) -> float:
+        return sum(flight.duration for flight in self.l_flights)
+
